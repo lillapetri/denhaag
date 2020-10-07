5217fbc3c777b81560c7e801ce7cfbe87dda4352
Lilla Petri <pelvaaf@gmail.com>
2020-10-07 12:26:42 +0200

    Clean up and rearrange file structure for deploying back-end API on Docker.

--- Models/price_category.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class PriceCategory(str, Enum):
-    free: str = "free"
-    cheap: str = "cheap"
-    average: str = "average"
-    expensive: str = "expensive"
--- Utils/db_object.py
+++
@@ -1,7 +0,0 @@
-from Utils.constants import DB_URL, IS_LOAD_TEST, TESTING, TEST_URL
-from databases import Database
-
-if TESTING or IS_LOAD_TEST:
-    db = Database(TEST_URL)
-else:
-    db = Database(DB_URL)
